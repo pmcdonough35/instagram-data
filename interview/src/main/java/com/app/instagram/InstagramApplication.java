43d2a5253e3838de91a3d8084b84b0286e2b9261
Patrick McDonough <pmcdonough@Patricks-MBP.fios-router.home>
2021-05-15 16:03:31 -0400

    unique apis

--- interview/src/main/java/com/app/instagram/InstagramApplication.java
+++ interview/src/main/java/com/app/instagram/InstagramApplication.java
@@ -1,41 +1,13 @@
 package com.app.instagram;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.client.RestTemplateBuilder;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.client.RestTemplate;
-
-import com.app.instagram.model.Account;
-import com.app.instagram.model.Post;
 
 @SpringBootApplication
 public class InstagramApplication {
-
-	private static final Logger log = LoggerFactory.getLogger(InstagramApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(InstagramApplication.class, args);
 	}
 
-	@Bean
-	public RestTemplate restTemplate(RestTemplateBuilder builder) {
-		return builder.build();
-	}
-
-	@Bean
-	public CommandLineRunner run(RestTemplate restTemplate) throws Exception {
-		return args -> {
-			Account quote = restTemplate.getForObject(
-					"https://www.instagram.com/instagram/channel/?__a=1", Account.class);
-			log.info(quote.toString());
-			
-			Post post = restTemplate.getForObject(
-					"https://www.instagram.com/p/B24tna0FM0I/?__a=1", Post.class);
-			log.info(post.toString());
-		};
-	}
 }
--- interview/src/main/java/com/app/instagram/controller/InstagramController.java
+++ interview/src/main/java/com/app/instagram/controller/InstagramController.java
@@ -2,14 +2,36 @@ package com.app.instagram.controller;
 
 
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.client.RestTemplate;
+
+import com.app.instagram.model.Account;
+import com.app.instagram.model.Post;
+
+import org.springframework.web.bind.annotation.GetMapping;
 
 @RestController
 public class InstagramController {
 
-	@RequestMapping("/")
-	public String index() {
-		return "Greetings ssdadfrom Spring Boot!";
+	private static final String GET_ACCOUNT = "https://www.instagram.com/{id}/channel/?__a=1";
+	private static final String GET_POST = "https://www.instagram.com/p/{post-id}/?__a=1";
+	
+	static RestTemplate restTemplate = new RestTemplate();
+	
+	@GetMapping("/api/account")
+	public static Account getAccount() {
+		Account account = restTemplate.getForObject(
+				"https://www.instagram.com/instagram/channel/?__a=1", Account.class);
+		return account;
 	}
+	
+	@GetMapping("/api/post")
+	public static Post getPost() {
+		Post post = restTemplate.getForObject(
+				"https://www.instagram.com/p/B24tna0FM0I/?__a=1", Post.class);  
+		return post;
+	}
+	
+	
+	
 }
 
